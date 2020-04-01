045619bd8c5f3f1c51c38a9e81f97020950537fd
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-01 11:31:17 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,14 +4,21 @@ if(!isset($_SESSION['user2'])){
  header("location:adminlogin.php?problem = not logged in");
  exit();
 }?>
-<?php
-mysql_connect("localhost","root","");
-mysql_select_db("library");
 
+<?php
+$hostname= 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username= 'jm5yt2mqq91oohli';
+$password= 'nijmbv3wx5tx9yto';
+$dbname= 'a3vhvpnccog050y7';
+
+$connect = mysqli_connect($hostname,$username,$password,$dbname) or die ("Error Connecting");
+?>
+
+<?php
 $delete_id = $_GET['del'];
 
 $query = "delete from user where id='$delete_id'";
-if(mysql_query($query)){
+if(mysqli_query($connect,$query)){
 echo "<script>alert('user has been deleted!')</script>";
 header("location:user.php");
 }
