632369b6f1fb9581e79f7b75c8b41b8fd6938bc5
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-01 11:40:07 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -10,10 +10,10 @@ $uid = $_SESSION['user'];
 ?>
 
 <?php
-$hostname= 'localhost';
-$username= 'root';
-$password= '';
-$dbname= 'library';
+$hostname= 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username= 'jm5yt2mqq91oohli';
+$password= 'nijmbv3wx5tx9yto';
+$dbname= 'a3vhvpnccog050y7';
 
 $connect = mysqli_connect($hostname,$username,$password,$dbname) or die ("Error Connecting");
 ?>
