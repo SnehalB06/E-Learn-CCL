17359b540aa44ff1d1c5dc65ceeaa83ac5c9fa84
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-01 12:10:56 +0530

    Update addbooks.php

--- addbooks.php
+++ addbooks.php
@@ -91,13 +91,30 @@ $connect = mysqli_connect($hostname,$username,$password,$dbname) or die ("Error 
 </html>
 
 <?php 
+	
+	
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
    if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $author = $_POST['author'];
 	$pub = $_POST['pub'];
 	$brn=$_POST['brn'];
 	$no = $_POST['no'];
-	
 	if($name==''){
 	echo "<script>alert('Please enter Book name!')</script>";
 	exit();
@@ -122,7 +139,7 @@ $connect = mysqli_connect($hostname,$username,$password,$dbname) or die ("Error 
    }
    
   
-   $query = "INSERT INTO `book` (`bid`, `bname`, `author`, `pub`, `bran`, `no`, `bimage`) VALUES (NULL, '$name', '$author', '$pub', '$brn', '$no', '')";
+   $query = "INSERT INTO `book` (`bid`, `bname`, `author`, `pub`, `bran`, `no`, `bimage`) VALUES (NULL, '$name', '$author', '$pub', '$brn', '$no', '$check')";
 	if(mysqli_query($connect,$query))
 	 header("Location:success.php");
 	 
