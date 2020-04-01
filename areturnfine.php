506aff6b786bca9d6fe69b4c6782ada83c063896
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-01 11:26:10 +0530

    Update areturnfine.php

--- areturnfine.php
+++ areturnfine.php
@@ -6,10 +6,10 @@ if(!isset($_SESSION['user2'])){
 }
 ?>
 <?php
-$hostname= 'localhost';
-$username= 'root';
-$password= '';
-$dbname= 'library';
+$hostname= 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username= 'jm5yt2mqq91oohli';
+$password= 'nijmbv3wx5tx9yto';
+$dbname= 'a3vhvpnccog050y7';
 
 $connect = mysqli_connect($hostname,$username,$password,$dbname) or die ("Error Connecting");
 ?>
