e73a3ab3cdbaae5cc6d552a97e2bc756705804ee
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-06 13:32:20 +0530

    Update adminhomepage.php

--- adminhomepage.php
+++ adminhomepage.php
@@ -31,12 +31,12 @@ if(!isset($_SESSION['user2'])){
   </li>
   <li><a href="#">MY ACTION</a>
     <ul>
-      <li><a href="abooks.php">Book Database</a></li>
-      <li><a href="addbooks.php">Add Book</a></li>
+     <li><a href="abooks.php">Book Database</a></li>
+      <li><a href="addbooks.php">Add Library Books</a></li>
       <li><a href="astatus.php">Issue Book</a></li>	
       <li><a href="areturnfine.php">Return Book</a></li>	  	  
-	  <li><a href="user.php">Member Database</a></li>
-      
+      <li><a href="user.php">Member Database</a></li>
+      <li><a href="auploadtry.php">Add E-books</a></li>
     </ul>
   </li>
 <!--
