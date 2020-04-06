54b48965f3ef0516a6c88f1f8558039d8588d5c3
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-06 13:57:30 +0530

    Update issue.php

--- issue.php
+++ issue.php
@@ -40,9 +40,9 @@ $usernm=$_SESSION['user'];
   </li>
   <li><a href="#">MY ACTION</a>
     <ul>
-      <li><a href="books.php">View Books</a></li>
+      <li><a href="books.php">View Library Books</a></li>
       <li><a href="issue.php">My Issue Status</a></li>
-      
+      <li><a href="dbookstry.php">Download E-Books</a></li>
     </ul>
   </li>
   <li><a href="#">STUDY RESOURCES</a>
