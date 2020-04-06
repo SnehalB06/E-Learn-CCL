c0640ccb64484e52c11e8311b83f4fdc51460c28
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-06 13:37:29 +0530

    Update agoals.php

--- agoals.php
+++ agoals.php
@@ -60,11 +60,11 @@ float:right;
   <li><a href="#">MY ACTION</a>
     <ul>
       <li><a href="abooks.php">Book Database</a></li>
-      <li><a href="addbooks.php">Add Book</a></li>
+      <li><a href="addbooks.php">Add Library Books</a></li>
       <li><a href="astatus.php">Issue Book</a></li>	
       <li><a href="areturnfine.php">Return Book</a></li>	  	  
 	  <li><a href="user.php">Member Database</a></li>
-      
+      <li><a href="auploadtry.php">Add E-books</a></li>
     </ul>
   </li>
   <!--
