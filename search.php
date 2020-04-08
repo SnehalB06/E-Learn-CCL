dc07bd75114258c411a5c6d09ab4d547517d61c5
SnehalB06 <43059934+SnehalB06@users.noreply.github.com>
2020-04-08 18:52:31 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,17 @@
+
+<?php
+$hostname= 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username= 'jm5yt2mqq91oohli';
+$password= 'nijmbv3wx5tx9yto';
+$dbname= 'a3vhvpnccog050y7';
+
+$connect = mysqli_connect($hostname,$username,$password,$dbname) or die ("Error Connecting");
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];}
+
+    echo $name;
+
+
+
+?>
